00eb67f3a5762db6a15712de67bb1e992cca77db
Sanjay Rautela <140192191+sanjayrautela@users.noreply.github.com>
2024-01-30 14:25:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your code here
+document.addEventListener('DOMContentLoaded', function () {
+    interact('.image')
+        .draggable({
+            listeners: {
+                start(event) {
+                    event.target.classList.add('selected');
+                },
+                move(event) {
+                    const target = event.target;
+                    const x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
+                    const y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+                    target.style.webkitTransform = target.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
+                    target.setAttribute('data-x', x);
+                    target.setAttribute('data-y', y);
+                },
+                end(event) {
+                    event.target.classList.remove('selected');
+                },
+            },
+        })
+        .on('dragmove', function (event) {
+            const target = event.target;
+            const x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
+            const y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+            target.style.webkitTransform = target.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
+            target.setAttribute('data-x', x);
+            target.setAttribute('data-y', y);
+        });
+});
+
